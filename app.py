e02fa9d580c328e49944d2b803558d4d5f66aca1
Aravind <aravindakumaran.v@gmail.com>
2022-11-22 11:44:29 +0100

    About me page added

--- app.py
+++ app.py
@@ -3,22 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-	# Load current count
-	f = open("count.txt","r")
-	count = int(f.read())
-	f.close()
+@app.route("/home")
+def home():
+	return render_template("home.html")
 
-	# increment the count
-	count +=1
-
-	# overwrite the count
-	f = open("count.txt","w")
-	f.write(str(count))
-	f.close()
-
-	# render HTML with count variable
-	return render_template("home.html", count=count)
+@app.route("/about")
+def about():
+	return render_template("about.html")
 
 if __name__ == "__main__":
 	app.run(debug=True)
